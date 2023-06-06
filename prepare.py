e3b0146f69026b493581a5235e83f77cd55db044
Jonathan Ware <jonathan.ware02@gmail.com>
2023-06-06 16:28:23 -0500

    added shape lines to prepare functions and reran notebook

--- prepare.py
+++ prepare.py
@@ -13,15 +13,16 @@ def split_titanic_data(df):
                                        random_state=123, 
                                        stratify=train_validate.survived)
     
+    print(f'Train:{train.shape}')
+    print(f'Test:{test.shape}')
+    print(f'Val: {validate.shape}')
     
     print("Train Titanic Data:")
-    print(train.head())
-
+    print(train)
     print("Test Titanic Data:")
-    print(test.head())
-
+    print(test)
     print("Validation Titanic Data:")
-    print(validate.head())
+    print(validate)
 
 def split_telco_data(df):
     
@@ -30,7 +31,18 @@ def split_telco_data(df):
                                        test_size=.3, 
                                        random_state=123, 
                                        stratify=train_validate.active)
-    return train, validate, test
+    
+    print(f'Train:{train.shape}')
+    print(f'Test:{test.shape}')
+    print(f'Val: {validate.shape}')
+
+    print("Train Telco Data:")
+    print(train.shape)
+    print(train)
+    print("Test Telco Data:")
+    print(test)
+    print("Validation Telco Data:")
+    print(validate)
 
 def split_iris_data(df):
     
@@ -39,4 +51,15 @@ def split_iris_data(df):
                                        test_size=.3, 
                                        random_state=123, 
                                        stratify=train_validate.species_setosa)
-    return train, validate, test
+    
+    print(f'Train:{train.shape}')
+    print(f'Test:{test.shape}')
+    print(f'Val: {validate.shape}')
+
+    print("Train Iris Data:")
+    print(train.shape)
+    print(train)
+    print("Test Iris Data:")
+    print(test)
+    print("Validation Iris Data:")
+    print(validate)
