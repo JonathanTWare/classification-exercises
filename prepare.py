3f4f6d0608076f5dc1f72ad90b73b07f58641b49
Jonathan Ware <jonathan.ware02@gmail.com>
2023-06-14 14:38:35 -0500

    made minor change to prep file and completed decision tree exercise

--- prepare.py
+++ prepare.py
@@ -3,6 +3,31 @@ import pandas as pd
 from pydataset import data
 from env import get_db_url
 import acquire
+import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
+
+
+#----------------------------------TITANIC-----------------------------------------------
+
+
+def clean_titanic_data(df):
+    
+    df = df.drop_duplicates()
+
+    
+    cols_to_drop = ['deck', 'embarked', 'class', 'passenger_id']
+    df = df.drop(columns=cols_to_drop)
+
+    
+    df['embark_town'] = df.embark_town.fillna(value='Southampton')
+
+    
+    dummy_df = pd.get_dummies(df[['sex', 'embark_town']], dummy_na=False, drop_first=[True])
+    df = df.drop(columns=['sex', 'embark_town'])
+    df = pd.concat([df, dummy_df], axis=1)
+
+    return df
 
 
 def split_titanic_data(df):
@@ -27,6 +52,71 @@ def split_titanic_data(df):
 
     return train, validate, test
 
+
+def impute_mean_age(train, validate, test):
+    
+    imputer = SimpleImputer(strategy = 'mean')
+    
+   
+    train['age'] = imputer.fit_transform(train[['age']])
+    
+   
+    validate['age'] = imputer.transform(validate[['age']])
+    
+    
+    test['age'] = imputer.transform(test[['age']])
+    
+    return train, validate, test
+
+def prep_titanic_data(df):
+    
+    df = clean_titanic_data(df)
+
+    train, validate, test = split_titanic_data(df)
+    
+    train, validate, test = impute_mean_age(train, validate, test)
+
+    return train, validate, test
+
+
+#----------------------------------TELCO----------------------------------------------
+def prep_telco_data(df):
+    df.drop(columns=['payment_type_id', 'internet_service_type_id', 'contract_type_id', 'customer_id'], inplace=True)
+       
+    df['total_charges'] = df['total_charges'].str.strip()
+    df = df[df.total_charges != '']
+    
+    df['total_charges'] = df.total_charges.astype(float)
+    
+    df['gender'] = df.gender.map({'Female': 1, 'Male': 0})
+    df['partner'] = df.partner.map({'Yes': 1, 'No': 0})
+    df['dependents'] = df.dependents.map({'Yes': 1, 'No': 0})
+    df['phone_service'] = df.phone_service.map({'Yes': 1, 'No': 0})
+    df['paperless_billing'] = df.paperless_billing.map({'Yes': 1, 'No': 0})
+    df['churn'] = df.churn.map({'Yes': 1, 'No': 0})
+    
+
+    
+    dummy_df = pd.get_dummies(df[['multiple_lines', \
+                              'online_security', \
+                              'online_backup', \
+                              'device_protection', \
+                              'tech_support', \
+                              'streaming_tv', \
+                              'streaming_movies', \
+                              'contract_type', \
+                              'internet_service_type', \
+                              'payment_type']], dummy_na=False, \
+                              drop_first=True)
+    
+    df = pd.concat([df, dummy_df], axis=1)
+    
+    train, validate, test = split_telco_data(df)
+    
+    return train, validate, test
+
+
+
 def split_telco_data(df):
     
     train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df.churn)
@@ -49,6 +139,22 @@ def split_telco_data(df):
 
     return train, validate, test
 
+
+
+
+#--------------------------------------IRIS---------------------------------------------
+def clean_iris(df):
+
+    df = df.drop(columns='species_id')
+    
+    df = df.rename(columns={'species_name':'species'})
+    
+    dummy_df = pd.get_dummies(df['species'], drop_first=False)
+    
+    df = pd.concat([df, dummy_df], axis=1)
+    
+    return df
+
 def split_iris_data(df):
     
     train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df.species)
@@ -70,3 +176,18 @@ def split_iris_data(df):
     print(validate)
 
     return train, validate, test
+
+def prep_iris_data(df):
+  
+    df = df.drop(columns='species_id')
+    
+    df = df.rename(columns={'species_name':'species'})
+    
+    dummy_df = pd.get_dummies(df['species'], drop_first=False)
+    
+    df = pd.concat([df, dummy_df], axis=1)
+    
+    train, validate, test = split_iris_data(df)
+    
+    return train, validate, test
+
